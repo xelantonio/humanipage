c42353b0af098cfe32ded36d723add5aaac1160f
xelantonio <65323556+xelantonio@users.noreply.github.com>
2022-09-10 14:40:12 -0600

    prueba dep

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './assets/scss/style.scss';
 const history = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={history}>
+  <Router  history={history}>
     <App />
   </Router>,
   document.getElementById('root')
