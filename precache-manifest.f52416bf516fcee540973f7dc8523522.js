118a419f3f52becf5cefb8daf7fc721154bde06b
xelantonio <65323556+xelantonio@users.noreply.github.com>
2022-09-10 13:52:15 -0600

    Updates

---
+++ precache-manifest.f52416bf516fcee540973f7dc8523522.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd138c9189e70cb6b9deb5949c52333c",
+    "url": "/humanipage/index.html"
+  },
+  {
+    "revision": "51531f2a9d5fc0c7963c",
+    "url": "/humanipage/static/css/main.be368e91.chunk.css"
+  },
+  {
+    "revision": "da066d407028cf67fecb",
+    "url": "/humanipage/static/js/2.d2eea5d3.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/humanipage/static/js/2.d2eea5d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51531f2a9d5fc0c7963c",
+    "url": "/humanipage/static/js/main.974f6e43.chunk.js"
+  },
+  {
+    "revision": "6f76497d3765051f6ce4",
+    "url": "/humanipage/static/js/runtime-main.b07df70d.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/humanipage/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "e4afd8441b52bb9d884afb35958477b9",
+    "url": "/humanipage/static/media/facebook.e4afd844.png"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/humanipage/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/humanipage/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/humanipage/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "653c7c4b8b881c3bc818a2d57ed48f1c",
+    "url": "/humanipage/static/media/femele.653c7c4b.jpg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/humanipage/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/humanipage/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "62840285c24f57fb3edeee6519178252",
+    "url": "/humanipage/static/media/instagram.62840285.png"
+  },
+  {
+    "revision": "ae00bd83a2868023f76929aa6929e05a",
+    "url": "/humanipage/static/media/logo.ae00bd83.png"
+  },
+  {
+    "revision": "42bc89e8eec3038f0d9b8e1e7cedbd6f",
+    "url": "/humanipage/static/media/mail.42bc89e8.png"
+  },
+  {
+    "revision": "d57ee08feda1dcb15665367f7a55012c",
+    "url": "/humanipage/static/media/telefono.d57ee08f.png"
+  },
+  {
+    "revision": "2c3dba13d72a6bb4713bb5f971cef0b3",
+    "url": "/humanipage/static/media/ubicacion.2c3dba13.png"
+  },
+  {
+    "revision": "7e87706461b1d934b715021c3f86bd66",
+    "url": "/humanipage/static/media/whatsapp.7e877064.png"
+  }
+]);
