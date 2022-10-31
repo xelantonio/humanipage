c8d449aa8f118fc995bb24a949f26f0c457bc2a4
xelantonio <65323556+xelantonio@users.noreply.github.com>
2022-10-31 16:33:09 -0600

    meta

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,17 +5,12 @@ import FeaturesTiles from '../components/sections/FeaturesTiles';
 import FeaturesSplit from '../components/sections/FeaturesSplit';
 import Testimonial from '../components/sections/Testimonial';
 import Cta from '../components/sections/Cta';
-import {Helmet} from "react-helmet";
+
 const Home = () => {
 
   return (
     <>
-    <Helmet>
-      <title> Laboratorio Humani</title>
-      <meta name="Laboratorio Humani, es un laboratorio clinico ubicado en Quetzaltenango cuyo principal objetivo es brindar un servicio de calidad y con calidez a nuestros pacientes" content=' En la pagina de laboratorio Humani encontras como contactarte con nosotros y nuestros servicios.'
-      />
     
-    </Helmet>
       <Hero className="illustration-section-01" />
    
       <FeaturesTiles />
