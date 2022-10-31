2026c52e4a7d044130a751a15f38683a350d12cf
xelantonio <65323556+xelantonio@users.noreply.github.com>
2022-10-31 17:18:59 -0600

    index

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const history = createBrowserHistory();
 ReactDOM.render(
   <Router  history={history}>
     <Helmet>
-      <title> Laboratorio Humani</title>
+      <title> Laboratorio Clinico Humani Xela</title>
       <link rel= "icon" type="image/png" sizes='32x32' href='.\public\isotipo.png'
       />
       <meta name="Laboratorio Humani, es un laboratorio clinico ubicado en Quetzaltenango cuyo principal objetivo es brindar un servicio de calidad y con calidez a nuestros pacientes" content=' En la pagina de laboratorio Humani encontras como contactarte con nosotros y nuestros servicios.'
